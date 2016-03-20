c11266f32b9b9726c5cb8af6ba91818e6ddcc4f4
JiwonZZang <choijiwon0911@gmail.com>
2016-03-20 17:05:25 +0000

    팔로우 중복 안되게 + 추천 마이필드에서 가능

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,9 +1,10 @@
 class FriendshipsController < ApplicationController
 	def create
-		@friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+		@friendship = current_user.friendships.find_or_create_by(:friend_id => params[:friend_id])
 		if @friendship.save
 			flash[:notice] = "Added friend."
-			redirect_to(:back)
+			#redirect_to(:back)
+			redirect_to '/my_feeld'
 		else
 			flash[:error] = "Unable to add friend."
 			redirect_to(:back)
