1f0e7bf78ea067e4df515a85614a00f7400445d9
JiwonZZang <jeinsong100@gmail.com>
2016-04-29 12:51:27 +0000

    assets.rb 수정

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,14 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( loadingoverlay.min.js )
-Rails.application.config.assets.precompile += %w( posts.js )
-Rails.application.config.assets.precompile += %w( posts.scss )
+# Rails.application.config.assets.precompile += %w( loadingoverlay.min.js )
+# Rails.application.config.assets.precompile += %w( posts.js )
+# Rails.application.config.assets.precompile += %w( posts.scss )
+
+
+# 참고하세요. Lucius => http://stackoverflow.com/a/12907744
+# Include all JS files, also those in subdolfer or javascripts assets folder
+# includes for exmaple applicant.js. JS isn't the problem so the catch all works.
+Rails.application.config.assets.precompile += %w(*.js)
+# Replace %w( *.css *.js *.css.scss) with complex regexp avoiding SCSS partials compilation
+Rails.application.config.assets.precompile += [/^[^_]\w+\.(css|css.scss)$/]
