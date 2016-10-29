62f50b2abb265d20542bec6887e3890f5815d385
JiwonZZang <jeinsong100@gmail.com>
2016-10-29 07:21:23 +0000

    반응형 요소 및 posts item 크기 조정

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -8,11 +8,13 @@ function postsColumnResize(){
 	//var pWidth = $('.posts-item').width() // 없을 땐 너비를 못찾아서절대값 넣음 
 	var pWidth = 215;  
 	var pMargin = 5 * 2;
+	if ($(window).width() > 400 ){
 		$('#posts-container')
 			.css('margin-left', ($('#posts-container').width() % (pWidth + pMargin))/2) 
 
 		$("#contents-controllbar")
 			.css('width', $('#contents-container').width())// contents container와 너비가 같게 해준다 
+	}
 }
 
 
